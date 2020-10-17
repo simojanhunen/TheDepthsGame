f9055923d89993fe1423a905191b258625c59890
simojanhunen <simo.janhunen@gmail.com>
2020-10-17 16:19:15 +0300

    [master] Add new tilemap blocks,
    Fix camera movement, i.e. remove stuttering
    Create new layouts

--- Assets/Scripts/CameraController2D.cs
+++ Assets/Scripts/CameraController2D.cs
@@ -6,15 +6,36 @@ public class CameraController2D : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset_from_character;
+    public float Smoothing = 0.13f;
+    public float Y_smoothing_multiplier = 2f;
 
     void Start()
     {
-        offset_from_character = player.transform.position - transform.position;
+        //offset_from_character = player.transform.position - transform.position;
     }
 
-    void LateUpdate()
+    void FixedUpdate()
     {
-        // TODO: smoothing without blurring character sprites
-        transform.position = player.transform.position - offset_from_character;  
+        // New target position for the camera
+        Vector3 targetPos;
+
+        // Calculate new X position with 1x smoothing.
+        targetPos.x = Mathf.MoveTowards(transform.position.x, player.transform.position.x, Smoothing);
+
+        // For some reason camera stuttered while running on even ground so added threshold for camera movement in Y-direction,
+        // the target position is calculated with parameterized smoothing multiplier, currently 2x smoothing.
+        if (Mathf.Abs(transform.position.y - player.transform.position.y) < 0.1)
+        {
+            targetPos.y = transform.position.y;
+        }
+        else
+        {
+            targetPos.y = Mathf.MoveTowards(transform.position.y, player.transform.position.y, Smoothing * Y_smoothing_multiplier);
+        }
+
+        // Z coordinate shouldn't come from player (2D game), so use cameras own.
+        targetPos.z = transform.position.z;
+
+        transform.position = targetPos;
     }
 }
